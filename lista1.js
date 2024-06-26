c2665fd5e89218625f40b25a3ec83a7a175f01e2
biancadutra1207 <110477085+biancadutra1207@users.noreply.github.com>
2024-06-26 11:40:39 -0300

    exercícios javaScript variáveis e arrayList

---
+++ lista1.js
@@ -0,0 +1,58 @@
+//exercicio 1
+let idade = 25;
+
+//ex 2
+let anoDeFormacao = 2025;
+
+//ex3
+var numero1 = 10;
+var numero2 = 5;
+var soma = numero1 + numero2;
+
+//ex4
+var num1 = 10;
+var num2 = 5;
+var sub = num1 - num2;
+
+//ex5
+var x = 3;
+x += 7;
+
+//ex6
+var y = 7
+
+//ex7
+function somaNumeros(a, b) {
+    return a + b;
+}
+
+//ex8
+function somaNumeros(a, b) {
+    console.log(a + b);
+}
+
+//ex9
+let frutas = ["maçã", "banana", "laranja"];
+console.log(frutas[1]); // banana
+
+//ex10
+let listasuoermercado = ["arroz", "feijão", "alface"];
+console.log(listasupermercado[0]); // arroz
+
+
+//ex11
+frutas[2] = "uva";
+console.log(frutas); // ["maçã", "banana", "uva"]
+
+//ex12
+listasupermercado[0] = "macarrão";
+console.log(listasupermercado[0]);
+
+
+
+
+
+
+
+
+
